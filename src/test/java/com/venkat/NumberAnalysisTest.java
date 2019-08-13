559b52841207e19b8de7e8031653d864c91bb695
venkatram.veerareddy <venkat.veerareddy@hotmail.com>
2019-08-13 16:06:20 +0530

    init commit

--- src/test/java/com/venkat/NumberAnalysisTest.java
+++ src/test/java/com/venkat/NumberAnalysisTest.java
@@ -3,12 +3,18 @@ package com.venkat;
 import com.venkat.service.NumberAnalysis;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class NumberAnalysisTest {
 
     private NumberAnalysis numberAnalysis;
+
+    @Rule
+    public ExpectedException thrownEx = ExpectedException.none();
 
     @Before
     public void setup() {
@@ -34,5 +40,13 @@ public class NumberAnalysisTest {
     @Test
     public void testZero(){
         assertEquals(numberAnalysis.numberInWords(0), "zero");
+    }
+
+    @Test
+    public void negativeNumberTest(){
+
+        thrownEx.expect(RuntimeException.class);
+        thrownEx.expectMessage("Negative number not valid.");
+        numberAnalysis.numberInWords(-5);
     }
 }
